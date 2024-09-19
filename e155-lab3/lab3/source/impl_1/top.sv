2124ac0059271af87b11fa68d72254e1e1e0fe00
alishachulani <alisha21@me.com>
2024-09-18 22:31:12 -0700

    Update top.sv

--- e155-lab3/lab3/source/impl_1/top.sv
+++ e155-lab3/lab3/source/impl_1/top.sv
@@ -17,12 +17,12 @@ module top (
         logic [3:0] val2;
         logic [3:0] sync_rows;
         sync sdut (int_osc, rows, sync_rows);
-        keypad inst1 (int_osc, reset, sync_rows, cols, val1, val2);
-        multiplexedSevenSeg inst2 (val2, val1, int_osc, reset, segments, power2, power1);
+        fsm inst1 (int_osc, reset, sync_rows, cols, val1, val2);
+        multiplexer inst2 (val2, val1, int_osc, reset, segments, power2, power1);
 
 endmodule
 
-module keypad (
+module fsm(
         input logic clk,
         input logic reset,
         input logic [3:0] rows,
@@ -56,8 +56,8 @@ module keypad (
         // Will use these as helpers when we enter debouncing stage
         logic [3:0] initialRows;
         logic [3:0] initialCols;
-        logic [8:0] counter;
-        logic [8:0] noKeyCounter;
+        logic [8:0] counterOn;
+        logic [8:0] counterOff;
         logic valid;
 
 
@@ -90,8 +90,8 @@ module keypad (
                         S8: if (rows == 4'b1110 || rows == 4'b1101 || rows == 4'b1011 || rows == 4'b0111) nextState <= S11;
                                 else nextState <= S0;
                         S11: nextState <= S9;
-                        S9: if (noKeyCounter == 9'b111111111 && valid) nextState <= S10;
-                                else if (noKeyCounter == 9'b111111111 && !valid) nextState <= S0;
+                        S9: if (counterOff == 9'b111111111 && valid) nextState <= S10;
+                                else if (counterOff == 9'b111111111 && !valid) nextState <= S0;
                                 else nextState <= S9;
                         S10: nextState <= S0;
                         default: nextState <= S0;
@@ -104,10 +104,9 @@ module keypad (
                         S2: cols <= 4'b1101;
                         S4: cols <= 4'b1011;
                         S6: cols <= 4'b0111;
-                        // Use S8 to initialize debouncing helper values
                         S8: begin
-                                counter <= 0;
-                                noKeyCounter <= 0;
+                                counterOn <= 0;
+                                counterOff <= 0;
                                 initialRows <= rows;
                                 initialCols <= cols;
                                 valid <= 0;
@@ -115,16 +114,16 @@ module keypad (
                         // Debounce the input
                         S9: begin
                                 if (rows == initialRows && cols == initialCols) begin // Realistically cols can't change at this point, but check anyways to be safe
-                                        noKeyCounter <= 0;
-                                        if (counter == 9'b111111111) begin
+                                        counterOff <= 0;
+                                        if (counterOn == 9'b111111111) begin
                                                 if (!valid) begin
                                                         valid <= 1;
                                                 end
-                                                counter <= counter;
-                                        end else counter <= counter + 1;
+                                                counterOn <= counterOn;
+                                        end else counterOn <= counterOn + 1;
                                 end else begin
-                                        if (noKeyCounter < 9'b111111111 && rows == 4'b1111) noKeyCounter <= noKeyCounter + 1;
-                                        else noKeyCounter <= 0;
+                                        if (counterOff < 9'b111111111 && rows == 4'b1111) counterOff <= counterOff + 1;
+                                        else counterOff <= 0;
                                 end
                         end
                         // Use S10 to assign new values
@@ -170,51 +169,47 @@ module sync (
 endmodule
 
 module sevenSeg (
-        input logic [3:0] S,
-        output logic [6:0] segments
+        input logic [3:0] switch,
+        output logic [6:0] seg
 );
         always_comb begin
-                case (S)
-                        4'b0000: segments = 7'b0000001;
-						4'b0001: segments = 7'b1001111;
-						4'b0010: segments = 7'b0010010;
-						4'b0011: segments = 7'b0000110;
-						4'b0100: segments = 7'b1001100;
-						4'b0101: segments = 7'b0100100;
-						4'b0110: segments = 7'b0100000;
-						4'b0111: segments = 7'b0001111;
-						4'b1000: segments = 7'b0000000;
-						4'b1001: segments = 7'b0000100;
-						4'b1010: segments = 7'b0001000;
-						4'b1011: segments = 7'b1100000;
-						4'b1100: segments = 7'b0110001;
-						4'b1101: segments = 7'b1000010;
-						4'b1110: segments = 7'b0110000;
-						4'b1111: segments = 7'b0111000;
+                case (switch)
+                        4'b0000: seg = 7'b0000001;
+						4'b0001: seg = 7'b1001111;
+						4'b0010: seg = 7'b0010010;
+						4'b0011: seg = 7'b0000110;
+						4'b0100: seg = 7'b1001100;
+						4'b0101: seg = 7'b0100100;
+						4'b0110: seg = 7'b0100000;
+						4'b0111: seg = 7'b0001111;
+						4'b1000: seg = 7'b0000000;
+						4'b1001: seg = 7'b0000100;
+						4'b1010: seg = 7'b0001000;
+						4'b1011: seg = 7'b1100000;
+						4'b1100: seg = 7'b0110001;
+						4'b1101: seg = 7'b1000010;
+						4'b1110: seg = 7'b0110000;
+						4'b1111: seg = 7'b0111000;
                 endcase
         end
 endmodule
 
 // Module to multiplex 2 7-segment displays using the sevenSeg module above
-module multiplexedSevenSeg (
+module multiplexer(
     input logic [3:0] S1,
     input logic [3:0] S2,
-        input logic int_osc,
-
+    input logic int_osc,
     input logic reset,
-
     output logic [6:0] segments,
-
     output logic power1,
     output logic power2
 );
 
-        // Use LSOSC help switch power back and forth between the 2 displays
 		logic [12:0] counter;
 
         logic [3:0] S;
 
-    sevenSeg seg1 (S, segments); // Instantiate seg1
+		sevenSeg seg (S, segments);
 
     always_ff @(posedge int_osc) begin
         if (reset == 0)
